c6feeff21958a0cdb86ac84d5b491e570ab035cf
workspacecgliterato <workspacecgliterato@gmail.com>
2026-08-02 00:40:30 +0800

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,11 @@ import { RenterReportsComponent } from './pages/renter-reports/renter-reports.co
 import { BookingReportsComponent } from './pages/booking-reports/booking-reports.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { RentersComponent } from './pages/renters/renters.component';
-import { LessorLoginComponent } from './pages/lessor-login/lessor-login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: RentersComponent
-  },
-  {
-    path:'login',
-    component: LessorLoginComponent
   },
   {
     path: 'renters/search-results',
